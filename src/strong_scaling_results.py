5ce6c3ab6d2d0766d708438af08772169b28a39e
RawanWagih <s-rawan.abdelfattah@zewailcity.edu.eg>
2025-11-29 19:52:48 +0200

    avoiding duplicates confusion in mpi times text file - handled by the python file

--- src/strong_scaling_results.py
+++ src/strong_scaling_results.py
@@ -1,20 +1,20 @@
-ranks = []
-times = []
+rank_time = {} 
 
 with open("mpi_times.txt", "r") as f:
     for line in f:
         rank, time = line.split()
-        ranks.append(int(rank))
-        times.append(float(time))
-
+        rank_time[int(rank)] = float(time)  # this overwrites duplicates --> keep the latest 
+        
+        
 #needed for calculations
-T1 = times[0] #from mpi_times.txt, time for 1 rank
-
+sorted_ranks = sorted(rank_time.keys())
+T1 = rank_time[1]  # baseline is rank 1
 
 print(f"{'Ranks':>5} {'Time(s)':>10} {'Speedup':>10} {'Efficiency(%)':>15}")
 
 # Compute speedup and efficiency 
-for i, t in enumerate(times):
+for r in sorted_ranks:
+    t = rank_time[r]
     speedup = T1 / t
-    efficiency = (speedup / ranks[i]) * 100
-    print(f"{ranks[i]:>5} {t:>10.5f} {speedup:>10.2f} {efficiency:>15.2f}")
+    efficiency = speedup / r * 100
+    print(f"{r:>5} {t:>10.5f} {speedup:>10.2f} {efficiency:>15.2f}")
