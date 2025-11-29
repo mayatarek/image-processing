c26d8eff1bcf229cf00a2542865d9838063b5dd7
RawanWagih <s-rawan.abdelfattah@zewailcity.edu.eg>
2025-11-29 21:39:55 +0200

    plot of weak scaling

--- src/weak_scaling_results.py
+++ src/weak_scaling_results.py
@@ -19,3 +19,19 @@ for r in sorted_ranks:
     sz = rank_size[r]
     print(f"{r:>5} {t:>10.5f} {sz:>15}")
 
+import matplotlib.pyplot as plt
+ranks = sorted_ranks
+times = [rank_time[r] for r in ranks]
+plt.figure()
+plt.plot(ranks, times, marker='o')
+
+ideal_time = times[0]
+plt.axhline(ideal_time, linestyle='--', label='Ideal Weak Scaling')
+
+plt.xlabel('Number of Processes (Ranks)')
+plt.ylabel('Execution Time (s)')
+plt.title('Weak Scaling: Execution Time vs Number of Processes')
+plt.xticks(ranks)
+plt.grid(True)
+plt.savefig('weak_scaling_plot.png')
+plt.show()
